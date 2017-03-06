4dc41e0e441fbbe21b1ec12c1e61bc2fce42b372
ro la <ro.la@hotmail.fr>
2017-03-06 10:28:20 +0000

    Setting different parameters to the autoencoder machine learning algorithm to compare the results in the improvement of noisy files

--- MatlabScriptsForBVH/addNoiseSelectedJoint.m
+++ MatlabScriptsForBVH/addNoiseSelectedJoint.m
@@ -1,9 +1,8 @@
-function [ skeleton_noisy ] = addNoiseSelectedJoint( skeleton, labelJoint, relativeDuration )
+function [ skeleton_noisy ] = addNoiseSelectedJoint( skeleton, labelJoint, relativeDuration, varNoise )
 %addNoiseSelectedJoint Create a copy of the skeleton, adding Gaussian Noise
 %on the selected joint for the indicated relative duration
 %   Detailed explanation goes here
 muNoise = 0;
-varNoise = 60;
 
 skeleton_noisy = skeleton;
 nbFrameOfNoise = floor(relativeDuration * skeleton.descriptions.nbFrames);
@@ -15,7 +14,10 @@ for i=1:skeleton.descriptions.nbChannels
 end
 noise = sqrt(varNoise).*randn(length(correspondingIndexes),nbFrameOfNoise) + muNoise;
 start = floor(rand(1,1)*(skeleton.descriptions.nbFrames - nbFrameOfNoise));
-skeleton.data(correspondingIndexes,start:start + nbFrameOfNoise - 1)= skeleton.data(correspondingIndexes,start:start + nbFrameOfNoise-1) + noise;
+if start<1
+   start = 1; 
+end
+skeleton_noisy.data(correspondingIndexes,start:start + nbFrameOfNoise - 1)= skeleton.data(correspondingIndexes,start:start + nbFrameOfNoise-1) + noise;
 
 end
 
--- MatlabScriptsForBVH/run.m
+++ MatlabScriptsForBVH/run.m
@@ -2,6 +2,12 @@ clear;
 configcmu
 
 nameJointAddNoise = 'foot';
+noiseRelativeDuration = 0.1; %1 means all the record will have added noise,
+                             %0 means no noise will be added, 0.5 means
+                             %half of the recording will have added noise..
+varNoise = 60; % variance of the Gaussian Noise which will be added
+nbMaxEpochs = 2000;
+sparsityProportion = 0.2;
 
 button = questdlg(strcat('The current body part on which noise is added is: "',nameJointAddNoise, '" do you want to change it ?'),'Change noisy body part','Yes','No','No');
 switch button
@@ -52,38 +58,64 @@ for i=1:length(train_bvhfiles)
     trainingSet  = [trainingSet skeleton{i}.data]; 
 end
 %Step3=> training a autoencoder nerual network to reconstruct the pose
-autoenc_rxyz = trainAutoencoder(trainingSet);
+autoenc_rxyz = trainAutoencoder(trainingSet,'ShowProgressWindow',false,'MaxEpochs',nbMaxEpochs,'SparsityProportion',sparsityProportion);
 
 
 %testing stage
 skeleton_test = cell(1,length(test_bvhfiles));
 skeleton_noisy = cell(1,length(test_bvhfiles));
-skeleton_predicted = cell(1,length(test_bvhfiles));
-error = cell(1,length(test_bvhfiles));
-for i=1:length(test_bvhfiles)
-    bvhfile= test_bvhfiles{i};
-    [skeleton_test{i}]=openBVH(bvhfile);%load bvh file and get all joints information and their corresponding timestamp
-    
-    %Todo:
-    %Step1=> downsampling from 120Hz to 30Hz since Kinect data is 30Hz and we would like to simluate the data as Kinect SDK obtained
-    skeleton_test{i}.data = skeleton_test{i}.data(:,1:4:end);
-    skeleton_test{i}.descriptions.nbFrames = size(skeleton_test{i}.data,2);
-    %Step2=> add noise to get corrupted joint information
-    [skeleton_noisy{i}] = addNoiseSelectedJoint(skeleton_test{i},nameJointAddNoise,0.1);
-    
-    %Step3=> using the autoencoder nerual network trained during training stage
-    testSet  = skeleton_noisy{i}.data; 
-    resultPredict = predict(autoenc_rxyz, testSet);
-    skeleton_predicted{i} = skeleton_test{i};
-    skeleton_predicted{i}.data = resultPredict;
-    error{i} = abs(resultPredict - testSet);
-end
+error_noisy = cell(1,length(test_bvhfiles));
+skeleton_predicted_once = cell(1,length(test_bvhfiles));
+error_once = cell(1,length(test_bvhfiles));
+skeleton_predicted_twice = cell(1,length(test_bvhfiles));
+error_twice = cell(1,length(test_bvhfiles));
 
 if ~exist(outputFolder,'dir')
     mkdir(outputFolder)
 end
 
-for i=1:length(skeleton_predicted)
-    bvhfile= strcat(outputFolder,'/',skeleton_predicted{i}.descriptions.nameFiles);
-    saveBVH(skeleton_predicted{i},bvhfile);
+for i=1:length(test_bvhfiles)
+    bvhfile= test_bvhfiles{i};
+    [skeleton_test{i}]=openBVH(bvhfile);%load bvh file and get all joints information and their corresponding timestamp
+    
+    %Step1=> downsampling from 120Hz to 30Hz since Kinect data is 30Hz and we would like to simluate the data as Kinect SDK obtained
+    skeleton_test{i}.data = skeleton_test{i}.data(:,1:4:end);
+    skeleton_test{i}.descriptions.nbFrames = size(skeleton_test{i}.data,2);
+    %Step2=> add noise to get corrupted joint information
+    [skeleton_noisy{i}] = addNoiseSelectedJoint(skeleton_test{i},nameJointAddNoise,noiseRelativeDuration, varNoise);
+    error_noisy{i} = abs(skeleton_noisy{i}.data - skeleton_test{i}.data); 
+    %Step3=> using the autoencoder neural network trained during training stage
+    testSet  = skeleton_noisy{i}.data; 
+    resultPredict = predict(autoenc_rxyz, testSet);
+    skeleton_predicted_once{i} = skeleton_test{i};
+    skeleton_predicted_once{i}.data = resultPredict;
+    error_once{i} = abs(resultPredict - skeleton_test{i}.data);
+    % Using the autoencoder neural network on the predicted value to see if
+    % it improves the results
+    testSet  = skeleton_predicted_once{i}.data; 
+    resultPredict = predict(autoenc_rxyz, testSet);
+    skeleton_predicted_twice{i} = skeleton_test{i};
+    skeleton_predicted_twice{i}.data = resultPredict;
+    error_twice{i} = abs(resultPredict - skeleton_test{i}.data);
+    
+    % Recording of the resulting skeleton in bvh format for visualization
+    bvhfile= strcat(outputFolder,'/once_noise_duration',num2str(noiseRelativeDuration*100),'_var_',num2str(varNoise),'_',skeleton_predicted_once{i}.descriptions.nameFiles);
+    saveBVH(skeleton_predicted_once{i},bvhfile);
+    bvhfile= strcat(outputFolder,'/twice_noise_duration',num2str(noiseRelativeDuration*100),'_var_',num2str(varNoise),'_',skeleton_predicted_twice{i}.descriptions.nameFiles);
+    saveBVH(skeleton_predicted_twice{i},bvhfile);
+    bvhfile= strcat(outputFolder,'/noise_duration',num2str(noiseRelativeDuration*100),'_var_',num2str(varNoise),'_',skeleton_noisy{i}.descriptions.nameFiles);
+    saveBVH(skeleton_noisy{i},bvhfile);
 end
+
+norm_error = zeros(length(skeleton_noisy),3);
+for i=1:length(skeleton_noisy)
+norm_error(i,1) = norm(error_noisy{i});
+norm_error(i,2) = norm(error_once{i});
+norm_error(i,3) = norm(error_twice{i});
+end
+boxplot(norm_error,'Labels',{strcat(num2str(noiseRelativeDuration*100),'%time',num2str(varNoise),' variance noisy data'),'predicted once', 'predicted twice'})
+xlabel('Type of data')
+ylabel('Norm of the error')
+ylim([0 1000])
+title('Norm  of the error between original data and the others')
+saveas(gcf,strcat('Epochs',int2str(nbMaxEpochs),'Sparsity',num2str(sparsityProportion*100),'percentNormOfTheErrorDuration',num2str(noiseRelativeDuration*100),'percentVariance',num2str(varNoise),'.png'));
